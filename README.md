52244b767a4de5107659a379c099ee7fb4a147d6
chibuezej <johnhenrychibueze@gmail.com>
2024-03-14 21:53:39 +0100

    tesing to do list with nextjs

---
+++ README.md
@@ -0,0 +1,6 @@
+# "Testing a Next.js App"
+How to use describe,it and expect
+
+## with Jest, React Testing Library, TypeScript
+
+This is a simple todo list that has the jest, reaact testing library installed
---
+++ src/app/__tests__/Home.test.tsx
@@ -0,0 +1,37 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Home from "../page";
+
+describe("Home", () => {
+  it("should add a new todo", async () => {
+    render(<Home />);
+    const input = screen.getByPlaceholderText("New Todo");
+    await userEvent.type(input, "My new todo");
+    expect(input).toHaveValue("My new todo");
+    const button = screen.getByRole("button", {
+      name: "Submit",
+    });
+    await userEvent.click(button);
+    expect(input).toHaveValue("");
+    const data = await screen.findByText("My new todo");
+    expect(data).toHaveTextContent("My new todo");
+  });
+
+  it("should update a todo", async () => {
+    render(<Home />);
+    const checkbox = screen.getAllByRole("checkbox")[0] as HTMLInputElement;
+    expect(checkbox.checked).toBeFalsy();
+    await userEvent.click(checkbox);
+    expect(checkbox.checked).toBeTruthy();
+  });
+
+  it("should delete a todo", async () => {
+    render(<Home />);
+
+    const todoText = screen.queryByText("Write Code 💻");
+    expect(todoText).toBeInTheDocument();
+    const button = screen.getAllByTestId("delete-button")[0];
+    await userEvent.click(button);
+    expect(todoText).not.toBeInTheDocument();
+  });
+});
